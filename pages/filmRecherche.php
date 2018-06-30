9743943e9970234d000c9d7ece9f9e0aa0881b73
nicolai <nicolai@dagestad.fr>
2018-06-30 20:25:54 +0200

    On utilise des tabulations pour indentergit add filmRecherche.php ! ( +simplification en 1 seule requette pour la recherhce de film par tag)

--- pages/filmRecherche.php
+++ pages/filmRecherche.php
@@ -1,84 +1,76 @@
-<?php session_start(); ?>
+<?php 
+session_start(); 
+include("db_connect.php");
+?>
 
 <!doctype html>
 
 
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Choix du film</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" type="image/x-icon" href="../images/icon.ico" />
-        <link rel="stylesheet" href="style/style.css" />
+	<head>
+		<meta charset="utf-8" />
+		<title>Choix du film</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" type="image/x-icon" href="../images/icon.ico" />
+		<link rel="stylesheet" href="style/style.css" />
 		<script src="../scripts/boite_dialogue.js" type="text/javascript"></script>	
-    </head>
-    
-    <body>
-        <!-- Haut de page -->
-        <?php include('header.php'); ?>
-        <main>
+	</head>
+	
+	<body>
+		<!-- Haut de page -->
+		<?php include('header.php'); ?>
+		<main>
 
 
 
-        <!-- On refuse l'accès si le visiteur n'est pas connecté -->
-        <?php
-            if ($_SESSION['statut'] != "admin" 
-            && $_SESSION['statut'] != "user") {
-                echo("<div class=\"error\">Vous devez être connecté pour accéder à cette page.</div>");
-                include('footer.html');
-                exit();
-            }
-         ?>        
-        <!-- On affiche un tableau des films -->
-        
-        
-        <table id="tableauFilms">
-            <?php 
-            $tagCherche = $_POST['tagCherche'];
-            echo " <h1>Resultat de la recherche pour : ".$tagCherche."</h1>";
-// TODO refractor this mess            
-			$requete = mysqli_prepare($link, "SELECT idTag FROM tags WHERE nomTag= ?");
+		<!-- On refuse l'accès si le visiteur n'est pas connecté -->
+		<?php
+			if ($_SESSION['statut'] != "admin" 
+			&& $_SESSION['statut'] != "user") {
+				echo("<div class=\"error\">Vous devez être connecté pour accéder à cette page.</div>");
+				include('footer.html');
+				exit();
+			}
+		 ?>		
+		<!-- On affiche un tableau des films -->
+		
+		
+		<table id="tableauFilms">
+			<?php 
+			$tagCherche = $_POST['tagCherche'];
+			echo " <h1>Resultat de la recherche pour : ".$tagCherche."</h1>";
+			$rqt = "SELECT films.idfilm,titre,anneesortie,realisateur, affiche
+					FROM films JOIN occurenceTags on films.idfilm=occurenceTags.idFilm 
+					JOIN tags USING(idTag)
+					WHERE nomTag=?";
+			$requete = mysqli_prepare($link, $rqt) or die(mysqli_error($link));
 			$requete->bind_param("s",$tagCherche);
 			$requete->execute();
-            while ($row = mysqli_fetch_assoc($requete)) {
-                $idTag=$row['idTag'];
-				$requete2 = mysqli_prepare($link, "SELECT idFilm FROM occurenceTags WHERE idTag= ?");
-				$requete2->bind_param("s",$idTag);
-				$requete2->execute();
-				while ($row2 = mysqli_fetch_assoc($requete2)) {
-					$idFilms=$row2['idFilm'];
-					$requete3 = mysqli_prepare($link,"SELECT * FROM films WHERE idfilm= ?");
-					$requete3->bind_param("s",$idFilms);
-					$requete3->execute();
-					while ($row3 = mysqli_fetch_assoc($requete3)) {
+			$requete->bind_result($idFilm, $titre, $annesortie, $realisateur, $affiche);
+				while ($requete->fetch()) {
 						if($i%3 == 0)
-						    echo "<tr>";
+							echo "<tr>";
 						?>
 						
-				    	<p>
-							<td>
-								<a href=<?php echo "lire_film.php?idfilm=".$row3['idfilm']; ?>>
-									<?php echo $row3['titre']; ?></br>
-									<img src="../images/<?php echo $row3['affiche']; ?>">
-								</a><br/>
-								<?php echo $row3['anneesortie']; ?><br/>
-								<?php echo $row3['realisateur']; ?><br/>
-							</td>
-						</p>
+						<td>
+							<a href=<?php echo "lire_film.php?idfilm=".$idfilm; ?>>
+								<?php echo $titre; ?></br>
+								<img src="../images/<?php echo $affiche; ?>">
+							</a><br/>
+							<?php echo $anneesortie; ?><br/>
+							<?php echo $realisateur; ?><br/>
+						</td>
 			
 						<?php
 						if($i%3==2)
 							echo "</tr>";
-						$i=$i+1;				
+						$i++;				
 					}
-				
-				}
-			}
 			?>
 		</table>
 		
 		</main>
 		<!-- Bas de page (mentions légales, ...) -->
 		<?php include('footer.html'); ?>
-    </body>
-</htlm>
+	</body>
+</html>
