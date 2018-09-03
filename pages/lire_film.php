1e08498861eb1cb091d718abe00c74f2d55b271f
nicolai <nicolai@dagestad.fr>
2018-09-04 00:18:36 +0200

    Changement de logo
    et correction de quelques petits bug

--- pages/lire_film.php
+++ pages/lire_film.php
@@ -67,7 +67,7 @@ include("db_connect.php");
 					else {
 	                	echo "<div class=\"info\">Ce tag existe deja.</div>";
     	            }
-                	$requete2->close();
+                	$requete1->close();
 					$rqt= mysqli_prepare($link,"SELECT idTag 
 												FROM tags 
 												WHERE nomTag= ?") or die(mysqli_error($link));
